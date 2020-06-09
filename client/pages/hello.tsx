9af2777934b31939ef7ed8b6b473da9bb8782ba5
Tolga Erdonmez <tolgaerdonmez@gmail.com>
2020-06-09 15:32:19 +0300

    fix hello page

--- client/pages/hello.tsx
+++ client/pages/hello.tsx
@@ -5,7 +5,9 @@ import Layout from "../components/Layout";
 function HelloPage(): ReactElement {
 	return (
 		<Layout>
-			<MeComponent>{({ data }) => <h1>Hello {data?.me.name}</h1>}</MeComponent>
+			<MeComponent>
+				{({ data, loading }) => (!loading && data?.me ? <h1>Hello {data?.me.name}</h1> : <h1>Loading...</h1>)}
+			</MeComponent>
 		</Layout>
 	);
 }
